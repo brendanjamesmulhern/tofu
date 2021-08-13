b9c129893a9ded02e5d37562509d2a9f5c3fdb57
earth-r <88467838+earth-r@users.noreply.github.com>
2021-08-13 18:07:39 -0400

    deploy

--- src/screens/GenerateLink.js
+++ src/screens/GenerateLink.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import axios from 'axios';
 
 const GenerateLink = () => {
 	const GenerateLink = async () => {
 		axios.get('https://api-tofu.herokuapp.com/getSessionAndToken').then(res => {
-			setSessionID(res['data']['sessionId']);
 			alert(`https://tofu-pied.vercel.app/${res['data']['sessionId']}/${res['data']['token']}`)
 		})
 	};
