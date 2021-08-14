f88d653533ceed608b85bd02e9884f8f06943d26
earth-r <88467838+earth-r@users.noreply.github.com>
2021-08-13 20:27:31 -0400

    deploy

--- src/screens/VideoChat.js
+++ src/screens/VideoChat.js
@@ -1,43 +1,55 @@
 import React, { useEffect, useState } from 'react';
-import * as OT from '@opentok/client';
+import { OTPublisher, createSession, OTSubscriber } from 'opentok-react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import RealmApp from '../config/RealmApp';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import OT from '@opentok/client';
 
 const secret = "fb557b9b880c278439a508c66dbb85be03552739";
 const apiKey = "47306554";
 
-const VideoChat = () => {
-	let { sessionID, token } = useParams;
-	useEffect(() => {
-		initTok();
-	}, []);
-	const initTok = () => {
-		var session = OT.initSession(apiKey, sessionID);
-		var publisher = OT.initPublisher('publisher', {
-			insertMode: 'append',
-			width: '100%',
-			height: '100%',
-		});
-		session.connect(token, (error) => {
-			session.publish(publisher);
-		});
-		session.on('streamCreated', function(event) {
-			session.subscribe(event.stream, 'subscriber', {
-				insertMode: 'append',
-				width: '100%',
-				height: '100%'
-			});
-		})
-	};
-	return (
-		<div id="video" className="h-full w-full flex flex-col">
-			<div className="h-96 w-screen" id='publisher'></div>
-			<div className="h-96 w-screen" id='subscriber'></div>
-		</div>
-	);
-};
+class VideoChat extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+    	streams: [],
+    	sessionId: this.props.match.params.sessionId,
+    	token: this.props.match.params.token
+    };
+  }
+  componentWillMount() {
+	console.log(this.props.match.params)
+    this.sessionHelper = createSession({
+      apiKey: apiKey,
+      sessionId: this.state.sessionId,
+      token: this.state.token,
+      onStreamsUpdated: streams => { this.setState({ streams }); }
+    });
+  }
+ 
+  componentWillUnmount() {
+    this.sessionHelper.disconnect();
+  }
+ 
+  render() {
+    return (
+      <div>
+        <OTPublisher session={this.sessionHelper.session} />
+ 
+        {this.state.streams.map(stream => {
+          return (
+            <OTSubscriber
+              key={stream.id}
+              session={this.sessionHelper.session}
+              stream={stream}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+}
 
 export default VideoChat;
