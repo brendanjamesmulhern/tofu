ac1e9205c18521def642ad465461b436e4a4370e
earth-r <88467838+earth-r@users.noreply.github.com>
2021-08-17 16:57:20 -0400

    deploy

--- src/screens/Account.js
+++ src/screens/Account.js
@@ -25,12 +25,12 @@ const Account = () => {
 			<Navbar />
 			<ul className="h-full flex flex-col justify-between my-5">
 				<div className="border border-gray-400 h-20 bg-white text-center">
-					<div className="my-5">Account Links</div>
+					<div className="my-5 text-2xl">Account Links</div>
 				</div>
 				{ arr ? arr.map(link => (
 					<li className="border border-gray-400 h-20 bg-white items-center justify-between">
 						<div>
-							<div className="text-center my-5"><a href={`/${link.address}`}>{link.name}</a></div>
+							<div className="text-md text-center my-6"><a href={`${link.address}`}>{link.name}</a></div>
 						</div>
 					</li>
 				)) : <div className="text-center my-10 text-xl">No Links</div> }
