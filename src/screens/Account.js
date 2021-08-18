e09e22e8c3cad81effe5a2584dd26a0e4216e183
earth-r <88467838+earth-r@users.noreply.github.com>
2021-08-18 17:01:36 -0400

    deploy

--- src/screens/Account.js
+++ src/screens/Account.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect} from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
-import { useHistory } from 'react-router-dom';'
+import { useHistory } from 'react-router-dom';
 
 let arr = [{
 	name: 'Upload Videos',
