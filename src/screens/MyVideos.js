ffe10edcf30ad74bb7d226ef9fa8d3b8a283d315
earth-r <88467838+earth-r@users.noreply.github.com>
2021-08-18 16:31:45 -0400

    deploy

--- src/screens/MyVideos.js
+++ src/screens/MyVideos.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import axios from 'axios';
 
 const MyVideos = () => {
+    let [videos, setVideos] = useState();
+    const getVideos = () => {
+
+    };
     return (
-        <div>
-            <div>My Videos</div>
+        <div className="flex flex-col justify-between h-screen bg-gray-200">
+            <Navbar />
+            <ul className="flex flex-col">
+        
+            </ul>
+            <Footer />
         </div>
     );
 };
