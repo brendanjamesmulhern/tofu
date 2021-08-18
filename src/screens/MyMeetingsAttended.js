f3dd74fa5cb34ab51f911dd71ebbd742f1013309
earth-r <88467838+earth-r@users.noreply.github.com>
2021-08-17 20:05:08 -0400

    deploy

--- src/screens/MyMeetingsAttended.js
+++ src/screens/MyMeetingsAttended.js
@@ -17,7 +17,11 @@ const MyMeetingsAttended = () => {
         <div className="flex flex-col h-screen justify-between bg-gray-200">
             <Navbar />
             <div>
-                <div>{console.log(meetings)}</div>
+                { meetings ? meetings.sort((a, b) => b.date - a.date ).map(meeting => (
+                    <div>
+                        
+                    </div>
+                )) : <div className="text-center text-xl mt-10">No Meetings</div>}
             </div>
             <Footer />
         </div>
